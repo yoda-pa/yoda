0f8bce9b4c5062afb240f4c48aef141792183658
Man Parvesh Singh Randhawa <manparvesh@users.noreply.github.com>
2017-02-13 18:30:13 +0800

    Update config.py, resolve #1

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ import platform
 API_AI_TOKEN = 'caca38e8d99d4ea6bd9ffa9a8be15ff9'
 API_AI_SESSION_ID = 'dd60fde7-c6ab-4f38-9487-7300c42b4916'
 
-# platform specific constants
+# these things work for all platforms
 PLATFORM = platform.system().lower()
-if PLATFORM == 'linux':
-    CONFIG_FILE_PATH = os.path.expanduser('~') + '/.dude/.dudeconfig.yml'
-    MONEY_CONFIG_FILE_PATH = os.path.expanduser('~') + '/.dude/money/.moneyconfig.yml'
-    DIARY_CONFIG_FILE_PATH = os.path.expanduser('~') + '/.dude/diary/.diaryconfig.yml'
+CONFIG_FILE_PATH = os.path.expanduser('~') + '/.dude/.dudeconfig.yml'
+MONEY_CONFIG_FILE_PATH = os.path.expanduser('~') + '/.dude/money/.moneyconfig.yml'
+DIARY_CONFIG_FILE_PATH = os.path.expanduser('~') + '/.dude/diary/.diaryconfig.yml'
