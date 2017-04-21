8be25bef76dc4b217ac4486d928bdecc1a92028c
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-04-21 15:39:39 +0800

    add NLP to money module

--- modules/money.py
+++ modules/money.py
@@ -1,10 +1,18 @@
 import click
 import chalk
 import yaml
+import apiai
+import config
+import json
 from forex_python.converter import CurrencyRates, CurrencyCodes
 
 from config import config_file_paths
 from util import *
+
+CLIENT_ACCESS_TOKEN = os.environ.get('API_AI_TOKEN', config.API_AI_TOKEN)
+ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+request = ai.text_request()
+request.session_id = os.environ.get('API_AI_SESSION_ID', config.API_AI_SESSION_ID)
 
 # config file path
 MONEY_CONFIG_FILE_PATH = config_file_paths['MONEY_CONFIG_FILE_PATH']
@@ -46,17 +54,25 @@ def setup():
 
     input_data(setup_data, MONEY_CONFIG_FILE_PATH)
 
+def expense():
+    request.query = raw_input()
+    click.echo('output: ')
+    response = request.getresponse().read()
+    output = json.loads(response)
+    click.echo(output)
+
 # command checker
 def check_sub_command(c):
     sub_commands = {
         'status' : status,
-        'setup' : setup
+        'setup' : setup,
+        'exp' : expense
     }
     try:
         return sub_commands[c]()
     except KeyError:
         chalk.red('Command does not exist!')
-        click.echo('Try "dude setup --help" for more info')
+        click.echo('Try "dude money --help" for more info')
 
 # the main process
 def process(input):
