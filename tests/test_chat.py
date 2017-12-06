43aa298f3ecb37157e95d737d23d86adc91c1e15
William Stevens <contact@wastevensv.com>
2017-12-05 20:43:03 -0500

    Adds test for chat command.
        Checks to confirm the command returns 0, as with SpeedTest example.

---
+++ tests/test_chat.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from unittest import TestCase
+from click.testing import CliRunner
+
+import yoda
+
+
+class TestChat(TestCase):
+    """
+        Test for the following commands:
+
+        | Module: chat
+        | command: chat
+    """
+
+    def __init__(self, methodName='runTest'):
+        super(TestSpeedtest, self).__init__()
+        self.runner = CliRunner()
+
+    def runTest(self):
+        result = self.runner.invoke(yoda.cli, ['chat', 'hello'])
+        self.assertEqual(result.exit_code, 0)
