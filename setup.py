93a5e5d7bb3cfcd09647054a9b52dcdbd90d7b89
AyushJha52 <cs15btech11008@iith.ac.in>
2017-12-14 20:36:18 +0530

    Can remove notes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='yoda',
     version='0.1.0',
-    py_modules=['yoda', 'whoosh'],
+    py_modules=['yoda'],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
