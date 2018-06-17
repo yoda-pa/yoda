ed2f1b6e7b750ab4e855b97fa6217f80a2cd219f
zuziik <zuzana@hromec.sk>
2018-06-17 23:11:57 +0200

    Added tests for goals module Updated tests for diary module

---
+++ tests/test_goals.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+from unittest import TestCase
+from click.testing import CliRunner
+
+import yoda
+
+
+class TestGoals(TestCase):
+    """
+        Test for the following commands:
+
+        Assumes empty goals list.
+
+        | Module: goals
+        | command: goals new, view, tasks, complete, analyze
+    """
+
+    def __init__(self, methodName='runTest'):
+        super(TestGoals, self).__init__()
+        self.runner = CliRunner()
+
+    def runTest(self):
+        result = self.runner.invoke(yoda.cli, ['goals'])
+        self.assertEqual(result.exit_code, 0)
+
+        result = self.runner.invoke(yoda.cli, ['goals', 'view'])
+        self.assertEqual(result.exit_code, 0)
+        result = self.runner.invoke(yoda.cli, ['goals', 'new'], input="test\ntest goal\n2020-02-02")
+        self.assertEqual(result.exit_code, 0)
+        result = self.runner.invoke(yoda.cli, ['goals', 'view'])
+        self.assertEqual(result.exit_code, 0)
+        result = self.runner.invoke(yoda.cli, ['goals', 'tasks'], input="test")
+        self.assertEqual(result.exit_code, 0)
+        result = self.runner.invoke(yoda.cli, ['goals', 'complete'], input="1")
+        self.assertEqual(result.exit_code, 0)
+        result = self.runner.invoke(yoda.cli, ['goals', 'analyze'])
+        self.assertEqual(result.exit_code, 0)
