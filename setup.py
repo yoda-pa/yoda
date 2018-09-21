b89fd067153ad7f8224760fc6bcce67effa86e87
Man Parvesh Singh Randhawa <manparvesh@users.noreply.github.com>
2018-09-21 14:37:00 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yoda',
-    version='0.2.0',
+    version='0.3.0',
     py_modules=['yoda'],
     packages=find_packages(),
     include_package_data=True,
