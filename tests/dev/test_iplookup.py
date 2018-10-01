680d1b38afa873d5f5e9477e5bead47f5be70c1d
Alexandru Văleanu <valeanu_alex@yahoo.com>
2018-10-01 11:36:17 +0300

    Add ip lookup functionality + test

---
+++ tests/dev/test_iplookup.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from unittest import TestCase
+from click.testing import CliRunner
+
+import yoda
+
+
+class TestIPLookup(TestCase):
+    """
+        Test for the following commands:
+
+        | Module: dev
+        | command: iplookup
+    """
+
+    def __init__(self, methodName='runTest'):
+        super(TestIPLookup, self).__init__()
+        self.runner = CliRunner()
+
+    def runTest(self):
+        result = self.runner.invoke(yoda.cli, ['iplookup', '128.101.101.101'])
+        output_string = str(result.output.encode('ascii', 'ignore').decode('utf-8')).strip()
+        self.assertTrue(output_string == 'Minnesota, United States')
