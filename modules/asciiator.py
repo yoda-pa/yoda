1c12325d7933dceb6b620a87b623a688e9b6f688
darkghost <dmytro@perkov.ca>
2018-10-08 14:50:41 -0400

    cleaning up asciiator.py file

--- modules/asciiator.py
+++ modules/asciiator.py
@@ -1,43 +1,5 @@
-from PIL import Image 
+from PIL import Image
 ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
-
-def scale_image(image, new_width=100):
-    """Resizes an image preserving the aspect ratio.
-    """
-    (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
-    new_height = int(aspect_ratio * new_width)
-
-    new_image = image.resize((new_width, new_height))
-    return new_image
-
-def convert_to_grayscale(image):
-    return image.convert('L')
-
-def map_pixels_to_ascii_chars(image, range_width=25):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
-
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
-
-    pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
-            pixels_in_image]
-
-    return "".join(pixels_to_chars)
-
-def convert_image_to_ascii(image, new_width=100):
-    image = scale_image(image)
-    image = convert_to_grayscale(image)
-
-    pixels_to_chars = map_pixels_to_ascii_chars(image)
-    len_pixels_to_chars = len(pixels_to_chars)
-
-    image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
-
-    return "\n".join(image_ascii)
 
 def handle_image_conversion(image_filepath):
     image = None
@@ -47,9 +9,27 @@ def handle_image_conversion(image_filepath):
         print ("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
         print(e)
         return
+    new_width=100
+    range_width=25
+    (original_width, original_height) = image.size
+    aspect_ratio = original_height/float(original_width)
+    new_height = int(aspect_ratio * new_width)
 
-    image_ascii = convert_image_to_ascii(image)
-    return  image_ascii
+    new_image = image.resize((new_width, new_height))
+    image = new_image
+    image = image.convert('L')
+
+
+    pixels_in_image = list(image.getdata())
+    pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
+            pixels_in_image]
+    pixels_to_chars = "".join(pixels_to_chars)
+    len_pixels_to_chars = len(pixels_to_chars)
+    image_ascii = [pixels_to_chars[index: index + new_width] for index in
+            xrange(0, len_pixels_to_chars, new_width)]
+
+    return "\n".join(image_ascii)
+
 def process(input_data):
 
     image_file_path = input_data
