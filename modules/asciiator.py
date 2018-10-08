4ae735c0fad2b8b8d4d39a22d7c05e2374fe0de0
darkghost <dmytro@perkov.ca>
2018-10-08 17:43:53 -0400

    fixes in asciiator

--- modules/asciiator.py
+++ modules/asciiator.py
@@ -1,5 +1,6 @@
 from PIL import Image
-ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
+ASCII_CHARS = ['#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
+
 
 def handle_image_conversion(image_filepath):
     image = None
@@ -9,26 +10,24 @@ def handle_image_conversion(image_filepath):
         print ("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
         print(e)
         return
-    new_width=100
-    range_width=25
+    new_width = 100
+    range_width = 25
     (original_width, original_height) = image.size
     aspect_ratio = original_height/float(original_width)
     new_height = int(aspect_ratio * new_width)
 
     new_image = image.resize((new_width, new_height))
-    image = new_image
-    image = image.convert('L')
-
+    image = new_image.convert('L')
 
     pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
-            pixels_in_image]
-    pixels_to_chars = "".join(pixels_to_chars)
+    pixels_to_chars = "".join([ASCII_CHARS[pixel_value/range_width] for pixel_value in
+            pixels_in_image])
     len_pixels_to_chars = len(pixels_to_chars)
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
             xrange(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
+
 
 def process(input_data):
 
