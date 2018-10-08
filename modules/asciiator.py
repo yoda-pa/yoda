f6fb8817e6929870bbb315e2e3c3c3f4e040f6e8
darkghost <dmytro@perkov.ca>
2018-10-08 18:41:53 -0400

    more cleaning in asciiator

--- modules/asciiator.py
+++ modules/asciiator.py
@@ -7,7 +7,7 @@ def handle_image_conversion(image_filepath):
     try:
         image = Image.open(image_filepath)
     except Exception as e:
-        print ("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
+        print("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
         print(e)
         return
     new_width = 100
@@ -15,15 +15,11 @@ def handle_image_conversion(image_filepath):
     (original_width, original_height) = image.size
     aspect_ratio = original_height/float(original_width)
     new_height = int(aspect_ratio * new_width)
-    new_image = image.resize((new_width, new_height))
-    image = new_image.convert('L')
-    pixels_in_image = list(image.getdata())
+    image = image.resize((new_width, new_height)).convert('L')
     pixels_to_chars = "".join([ASCII_CHARS[pixel_value/range_width] for pixel_value in
-            pixels_in_image])
-    len_pixels_to_chars = len(pixels_to_chars)
+            list(image.getdata())])
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
-
+            xrange(0, len(pixels_to_chars), new_width)]
     return "\n".join(image_ascii)
 
 
