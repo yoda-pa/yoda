bb13edec072845a9f826da41ced1aaeb23a01d99
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2019-01-12 03:00:07 +0800

    prepare for v0.4.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yoda",
-    version="0.3.0",
+    version="0.4.0",
     py_modules=["yoda"],
     packages=find_packages(),
     include_package_data=True,
